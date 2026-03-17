3c7886c8f9e19a145f5e74a13788eddb18f44c52
alexvaloc <alejandravalenciaocampo@gmail.com>
2026-03-17 19:52:55 +0100

    paginación terminada

--- 4.1 - Frameworks - React/src/header.tsx
+++ 4.1 - Frameworks - React/src/header.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export const Header: React.FC = () => {
   return (
     <header className="page-header">
-      <h1>Laboratorio 4.1 - Frameworks - React</h1>
+      <h1 className="project-title">Org Members Viewer</h1>
+      <p className="project-subtitle">
+        Busca miembros de organizaciones en GitHub con filtrado y paginación
+      </p>
     </header>
   );
 };
--- 4.1 - Frameworks - React/src/list.tsx
+++ 4.1 - Frameworks - React/src/list.tsx
@@ -10,17 +10,28 @@ interface Props {
 export const List: React.FC<Props> = ({ filter }) => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const [debouncedFilter] = useDebounce(filter, 500);
+  //página actual:
+  const [page, setPage] = React.useState(1);
+  //tamaño de página:
+  const [perPage] = React.useState(5);
+  const hasNextPage = members.length === perPage;
 
   React.useEffect(() => {
-    if (!debouncedFilter || debouncedFilter.length < 3) return;
+    // if (!debouncedFilter || debouncedFilter.length < 3) return;
 
+    const url = `https://api.github.com/orgs/${debouncedFilter}/members?page=${page}&per_page=${perPage}`;
     try {
-      fetch(`https://api.github.com/orgs/${debouncedFilter}/members`)
+      fetch(url)
         .then((response) => response.json()) //response es un objeto, lo parseamos a json
-        .then((json) => setMembers(json));
+        .then((json) => setMembers(json)); //guardamos resultado en estado
     } catch (e) {
       console.log("Error loading members");
     }
+  }, [debouncedFilter, page, perPage]);
+
+  React.useEffect(() => {
+    //Si el filtro cambia, volvemos a página 1
+    setPage(1);
   }, [debouncedFilter]);
 
   return (
@@ -34,6 +45,25 @@ export const List: React.FC<Props> = ({ filter }) => {
         {members.map((member) => (
           <MemberRow key={member.id} member={member} filter={debouncedFilter} />
         ))}
+        <div className="pagination">
+          <button
+            className="pagination-button"
+            //Calcula la nueva página usando el valor real más reciente:
+            onClick={() => setPage((prevPage) => prevPage - 1)}
+            disabled={page === 1}
+          >
+            Anterior
+          </button>
+          <span className="pagination-info">Página {page}</span>
+          <button
+            className="pagination-button"
+            onClick={() => setPage((prevPage) => prevPage + 1)}
+            // si no hay más resultados, se desactiva
+            disabled={!hasNextPage}
+          >
+            Siguiente
+          </button>
+        </div>
       </div>
     </>
   );
